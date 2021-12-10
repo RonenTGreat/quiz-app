248563c81994deebc39504f5de6277eed6b2a7a8
ronentgreat <ronenh53@gmail.com>
2021-12-10 18:12:06 +0000

    added question

---
+++ script.js
@@ -0,0 +1,42 @@
+const quizData = [
+  {
+    question: "How old is Ronen?",
+    a: "10",
+    b: "17",
+    c: "23",
+    d: "19",
+    correct: "c",
+  },
+  {
+    question: "What is the most used programming language in 2019?",
+    a: "Java",
+    b: "C",
+    c: "Python",
+    d: "JavaScript",
+    correct: "d",
+  },
+  {
+    question: "Who is the President of Ghana?",
+    a: "Ofori Atta",
+    b: "Nana Addo",
+    c: "Donald Trump",
+    d: "John Mahama",
+    correct: "b",
+  },
+  {
+    question: "What is does HTML stand for?",
+    a: "Hypertext Markup Language",
+    b: "Cascading Style Sheet",
+    c: "JavaScript Object Notation",
+    d: "Helicopters Terminals Motorboats Lamborginis",
+    correct: "a"
+  },
+  {
+      question: "What year was JavaScript launched?",
+      a: "1996",
+      b: "1995",
+      c: "1994",
+      d: "None of the above",
+      correct: "d"
+  }
+];
